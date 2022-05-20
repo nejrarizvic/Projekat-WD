cbc0e68173e519a250b6690b9afdf25a9c40a3b5
nejrarizvic <nejra.rizvic.20@size.ba>
2022-05-20 12:30:58 +0200

    git push--all origin

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+const BASE_URL = "https://ptf-web-dizajn-2022.azurewebsites.net/books";
+
+
+
+
+
 fetch('https://ptf-web-dizajn-2022.azurewebsites.net/books')
 .then(res => {
     if(!res.ok) {
@@ -20,7 +26,7 @@ fetch('https://ptf-web-dizajn-2022.azurewebsites.net/books')
                     <p class="card-text text-dark">Author: ${element.author.name} </p>
                     <div class="card-overlay">
                     </div>
-                    <button type="button" class="btn btn-secondary" href="" class="btn content__books__cards__root-buttons" onclick="deletebook(this)" id="content__books__cards__root-buttons-delete"> <span class="text-light">Delete book</span></button>
+                    <button type="button" class="btn btn-secondary" href="" class="btn content__books__cards__root-buttons" onclick="deleteBooks(this)" id="content__books__cards__root-buttons-delete"> <span class="text-light">Delete book</span></button>
                     <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@getbootstrap">Reservation book</button>
 
                     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -74,6 +80,83 @@ fetch('https://ptf-web-dizajn-2022.azurewebsites.net/books')
     cardList.innerHTML = cards; 
 })
 
+
+
+const deleteBooks = (books) => {
+  let booksId = books.parentElement.parentElement.id;
+  
+  fetch(`https://ptf-web-dizajn-2022.azurewebsites.net/books/${booksId}`, {
+      method: 'DELETE'
+  })
+  .then(res => {
+      if (res.ok) {
+          console.log(`Status code: ${res.status}`);
+          
+          let booksCard = document.getElementById(booksId);
+         booksCard.remove();
+          
+      } else {
+          let popup = document.querySelector('#error-pop-up');
+      }
+  })
+}
+
+
+const addBook = () => {
+  const bookName = document.getElementById('book-name').value;
+  
+  fetch(`${BASE_URL}/api/books/new`, {
+      method: 'POST',
+      headers: new Headers({'content-type': 'application/json'}),
+      body: JSON.stringify({
+          name: bookName
+      })
+  })
+  .then(res => {
+      console.log(res);
+  })
+}
+
+
+/*
+const fillEditData = (bookId) => {
+  const book = books.find(book => book.id === bookId);
+  const bookFormId = document.getElementById('book-id');
+  const bookFormName = document.getElementById('book-name');
+  const bookFormImage = document.getElementById('book-image');
+  const bookFormAuthor = document.getElementById('book-author');
+
+  bookFormId.value = book.id;
+  bookFormName.value = book.name;
+  bookFormImage.value = book.imageUrl;
+  bookFormAuthor.value = book.author;
+}
+
+const editBook = () => { 
+  const bookFormId = document.getElementById('book-id').value;
+  const bookFormName = document.getElementById('book-name').value;
+  const bookFormImage = document.getElementById('book-image').value;
+  const bookFormAuthor = document.getElementById('book-author').value;
+
+fetch(`${BASE_URL}/api/Book`, {
+  method: 'PUT', 
+  headers: new Headers({'content-type': 'application/json'}),
+  body: JSON.stringify({
+      id: bookFormId,
+      name: bookFormName,
+      imageUrl: bookFormImage,
+      author: bookFormAuthor
+  })
+})
+.then(res => {
+  if(!res.ok)
+  {
+      alert('Error');
+  }
+})
+}
+
+*/
 
 
 
